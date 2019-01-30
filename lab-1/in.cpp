46353c740c745150b0ab7370a17c4fc57b6691b0
Anand Karra <i7andgravity@live.com>
2019-01-30 19:03:53 +0530

    Added input file for Lab 1

---
+++ lab-1/in.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a,b; //input variables
+	cin >> a >> b; //take input from stdin
+	cout << a+b*a << endl;
+	/*
+	cout << a+b;
+	*/
+	return 0;
+}
